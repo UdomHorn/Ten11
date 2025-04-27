12f96d1da24bf92d2e4f6cb7b65ea0146ffceb23
Genus_ <udomhorn04@gmail.com>
2025-04-27 12:20:12 +0700

    recreate component use class component

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -14,6 +14,38 @@ import img11 from '../assets/Images/IMG_0162 copy-cr-450x672(1).jpg'
 import { useEffect, useState } from 'react'
 import {faBookmark} from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { div, h2 } from 'framer-motion/client'
+
+const Collection = ({src,title}) => {
+  return (
+    <div>
+          <img src={src} alt="" />
+          <div className='flex justify-center items-center text-center mt-3'>
+          <div className=' w-[94%] font-roboto font-bold text-base p-3  border border-gray-500 '>{title}</div>
+          </div>
+        </div>
+  )
+    
+}
+
+const HightLightCard = ({src,price,title}) =>{
+  return(
+    <div>
+            <img src={src} alt="" />
+            <div className='mt-3 p-1'>
+            <div className='flex justify-between '>
+              <div className='font-bold text-base '>
+                {price}
+              </div>
+              <div className='text-gray-400  '>
+              <FontAwesomeIcon icon={faBookmark} />
+              </div>
+            </div>
+            <p className='text-sm'>{title}</p>
+            </div>
+          </div>
+  )
+}
 
 const images = [img1, img12]
 
@@ -30,7 +62,8 @@ const Home = () => {
 
   return (
     <div className='pt-[48px] font-roboto'>
-      <div>
+
+      {/* <div>
         <div> 
           <img src={images[currentIndex]}
            alt={`$currentIndex + 1`} 
@@ -182,7 +215,44 @@ const Home = () => {
         </div>
         </div>
 
+      </div> */}
+
+      <div> 
+          <img src={images[currentIndex]}
+           alt={`$currentIndex + 1`} 
+           className="w-full object-cover transition-all duration-3000"
+           />
+        </div>
+      <div className='w-[75%] max-md:w-[94%] h-full  flex justify-center items-center m-auto mt-4 gap-7'>
+        <Collection src={img2} title="Women Collection"/>
+        <Collection src={img3} title="Men Collection"/>
       </div>
+
+      <div className='w-full text-3xl font-bold mt-10 px-1.5 py-2.5 '>
+          This Week's Hilights
+        </div>
+
+        <div className='grid grid-cols-2 max-sm:grid-cols-1 gap-4 px-4'>
+          <div className='flex gap-4  '>
+          <HightLightCard src={img4} price="$12.59" title="Regular Fitted double T-Shirt" />
+          <HightLightCard src={img5} price="$18.59" title="Tartan Maxi Skirt" />
+          </div>
+          <div className='flex gap-4  '>
+          <HightLightCard src={img6} price="$20.95" title="Kniited Fitted Cardigan" />
+          <HightLightCard src={img7} price="$17.59" title="Gathering Midi Dress" />
+          </div>
+        </div>
+
+        <div className='grid grid-cols-2 max-sm:grid-cols-1 gap-4 px-4 mt-20'>
+          <div className='flex gap-4  '>
+          <HightLightCard src={img8} price="$21.59" title="Wide Leg Denim Jean" />
+          <HightLightCard src={img9} price="$15.95" title="Loose Fit Dyat T-Shirt" />
+          </div>
+          <div className='flex gap-4  '>
+          <HightLightCard src={img10} price="$18.95" title="Loose Fit T-Shirts With Printed" />
+          <HightLightCard src={img11} price="$21.59" title="Wide Leg Loose Fit Jean." />
+          </div>
+        </div>
     </div>
   )
 }
