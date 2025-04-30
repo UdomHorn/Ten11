d2bf446f3c4f97ad69facf0dc84cb4278e77ad0e
Genus_ <udomhorn04@gmail.com>
2025-04-30 16:40:15 +0700

    continue women1

--- src/Component/Women1.jsx
+++ src/Component/Women1.jsx
@@ -8,6 +8,7 @@ import img4 from '../assets/Images/Women/Women1/ZANDO01.04.202511292.jpg'
 import img5 from '../assets/Images/Women/Women1/ZANDO01.04.202511293.jpg'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 const images = [img2, img3, img4, img5]
+
 const Women1 = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
   useEffect(() => {
@@ -19,8 +20,8 @@ const Women1 = () => {
   }, []);
   return (
     <div className='  p-2.5 pt-[48px] font-roboto w-[80%] max-md:w-full mx-auto text-lg '>
-      <div className='flex gap-8'>
-        <div className='w-1/2  '>
+      <div className='sm:flex gap-8'>
+        <div className='w-1/2 max-sm:w-full  '>
           <div>
             <img src={images[currentIndex]}
               alt={`$currentIndex + 1`}
@@ -28,7 +29,7 @@ const Women1 = () => {
             />
           </div>
         </div>
-        <div className='w-1/2'>
+        <div className='w-1/2 max-sm:w-full'>
           <div className='flex justify-between items-center'>
             <div>
               <p>Regular Fitted double T-Shirt</p>
@@ -41,15 +42,43 @@ const Women1 = () => {
             </div>
           </div>
           $12.59
-          <div className=' mt-4 font-bold text-xl'>1 Color available</div>
+          <div className=' my-4 font-bold text-xl'>1 Color available</div>
 
-          <div className='w-[160px] text-center'>
+          <div className='w-[160px]  max-sm:w-[100px] text-center '>
             <div className=' border border-black'>
               <img src={img1} alt="" />
 
             </div>
-            <div>Black</div>
+            <div className='text-sm my-4'>Black</div>
           </div>
+
+          <div className='flex justify-between items-center my-4'>
+            <div className='text-2xl font-bold'>Size</div>
+          <div className='text-sm underline cursor-pointer'>Size Guide</div>
+          </div>
+
+         
+           <div className='flex text-center gap-2 flex-wrap pb-6'>
+           <div className='w-[70px] p-2 bg-gray-100 
+             cursor-pointer'>XS</div>
+            <div className='w-[70px] p-2 bg-gray-100 
+             cursor-pointer'>S</div>
+            <div className='w-[70px] p-2 bg-gray-100 
+             cursor-pointer'>M</div>
+            <div className='w-[70px] p-2 bg-gray-100 
+             cursor-pointer'>L</div>
+            <div className='w-[70px] p-2 bg-gray-100 
+             cursor-pointer'>XL</div>
+           </div>
+           <div className='flex gap-2 items-center'>
+            <div className='w-4 h-4 bg-black'></div>
+            <div className='text-sm text-gray-700'>
+            Model is 161 cm tall / 43 kg weight and is wearing size S.
+            </div>
+           </div>
+
+           <div className='text-2xl font-bold py-6'>Qty</div>
+         
 
         </div>
 
